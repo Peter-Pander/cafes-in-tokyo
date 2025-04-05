7687909e57b9cd6acd6384ec14e2ffde1c4c2689
Maximilian Schöpf <172821797+Peter-Pander@users.noreply.github.com>
2025-04-06 00:53:14 +0200

    Get all the cafes from API

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -3,12 +3,25 @@
 import React from 'react';
 import './Searchbar.css';
 
-function Searchbar() {
+function Searchbar({ setCafeDatas }) {
+
+  const getCafes = (event) => {
+    console.log("Getting cafes...");
+    const keyword = event.currentTarget.value;
+    const url = `https://matcha-and-keyboard-f549965e60e7.herokuapp.com/api/v1/cafes?title=${keyword}`;
+    fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      console.group(data);
+      // cafeDatas = data;
+      setCafeDatas(data);
+    });
+  };
 
   return (
     <div className="searchbar input-group">
       <span className="input-group-text"><i className="fa-solid fa-magnifying-glass"></i></span>
-      <input placeholder="Starbucks" type="text" className="form-control" />
+      <input onChange={getCafes} placeholder="Starbucks" type="text" className="form-control" />
     </div>
   );
 }
